dace8ba605c3352fe5a96492b9049e2aad2dc75b
ARUN <arunjdev26@gmail.com>
2025-12-09 17:39:01 +0530

    new

--- src/pages/PreIPOStocks.jsx
+++ src/pages/PreIPOStocks.jsx
@@ -40,9 +40,7 @@ const PreIPOStocks = () => {
             <h2 className="text-2xl sm:text-3xl font-bold text-gray-900">
               Pre-IPO Stocks
             </h2>
-            <p className="text-gray-600 text-sm sm:text-base mt-1">
-              Exclusive Unlisted Market Opportunities
-            </p>
+          
           </div>
 
           {/* TABLE WRAP */}
