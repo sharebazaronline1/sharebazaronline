17c360e94921d82f6e08f9fbbbdaadd6ad70692c
ARUN <arunjdev26@gmail.com>
2026-06-29 14:29:07 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -50,7 +50,6 @@ import BrokerCompare from "./pages/BrokerCompare";
 import AdminDividends from "./pages/AdminCorporateActions";
 import CorporateActions from "./pages/CorporateActions"
 import BrokerReviewDetail from "./pages/BrokerDetails";
-import AdminBrokerReview from "./pages/AdminBrokerReview";
 // Optional: If you want admin to have its own layout without user sidebar/header
 function AdminLayout() {
   const location = useLocation();
@@ -158,7 +157,6 @@ const hideHeader =
                 <Route path="/admin-blogs" element={<AdminBlog />} />
                 <Route path="/admin-signals" element={<AdminSignals />} />
                 <Route path="/admin-corporate" element={<AdminDividends />} />
-                 <Route path="/admin-broker" element={<AdminBrokerReview />} />
                 
               </Route>
 
